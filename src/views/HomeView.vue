4f24567b9c1e898667d94573be8eb5e8b7922d96
Seedorf-Rai <dorf123see@gmail.com>
2022-09-24 21:33:13 +0530

    some changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>This is Home page</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est itaque voluptatum odit. Ducimus fugiat suscipit esse impedit perspiciatis odio animi, illo repellat consequatur amet similique consectetur atque quos maiores maxime!
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
+
+<style>
+
+</style>
