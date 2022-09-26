acd63865c552efd60430eb478632b0a15f31197e
Seedorf-Rai <dorf123see@gmail.com>
2022-09-26 12:28:10 +0530

    hero left image added wirh border and bg

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,8 +5,10 @@
     <!-- Hero Section -->
     <div class="container-fluid">
       <div class="row">
-        <div class="col-md-">
-          <img src="/images/hoodie2.png" alt="">
+        <div class="col-md-5  hero-left ">
+           <div class="hero-bg-color "></div>
+           <div class="hero-right-outline"></div>
+          <img src="/images/hoodie2.png" class="hero-img" alt="">
         </div>
       </div>
     </div>
@@ -22,5 +24,33 @@ export default {
 </script>
 
 <style>
-
+.hero-img{
+  width: 450px;
+  height: 500px;
+  object-fit: cover;
+  z-index: 100000;
+  position: absolute;
+  left: 24%;
+}
+.hero-bg-color{
+  background:#ba4826;
+  transform: skew(12deg);
+  position: absolute;
+  height: 450px;
+  width: 400px;
+  top: 50px;
+  left: 27%;
+}
+.hero-right-outline{
+  border: 2px solid #ba4826;
+  transform: skew(12deg);
+  position: absolute;
+  height: 450px;
+  width: 400px;
+  top: 80px ;
+  left: 33%;
+}
+.hero-left{
+  position: relative;
+}
 </style>
